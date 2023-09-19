80dfc238c6a01c87a822969330fc1d7747a6b2a7
NinoGvimradze <ngvimradze3@gmail.com>
2023-09-19 23:22:34 +0400

    2

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ let test=100;
 if(test < 50){console.log("<50")}
 else if(test > 20){console.log(">20")}
 else{console.log("false")}
+
+let box = "mariam" ;
+if(box === "mariam"){console.log("true")}
+else{console.log("false")}
